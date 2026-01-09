aafc95a5aba6d8ee8447a96119280be0889d6ae1
Vic-Nas <nasci.victorio@gmail.com>
2026-01-09 05:12:32 -0500

    Fixed profile search and added favicon

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('profile/<str:username>/', views.publicProfile, name='publicProfile'),
     path('profile/search/', views.profileSearch, name='profileSearch'),
+    path('profile/<str:username>/', views.publicProfile, name='publicProfile'),
 ]
